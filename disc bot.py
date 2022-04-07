4ca4d368ddab98ba81e54a926b491bef44c7b832
yshelev <95019066+yshelev@users.noreply.github.com>
2022-04-07 15:05:19 +1000

    Add files via upload

---
+++ disc bot.py
@@ -0,0 +1,73 @@
+import asyncio
+import discord
+from discord.ext import commands
+import random
+from pprint import pprint
+
+TOKEN = ""
+bot = commands.Bot(command_prefix='!')
+
+current_task = '-1'
+all_tasks = {}
+with open('егэ по физике задания.txt', encoding='utf-8', mode="r") as f:
+    tasks = f.read()
+    tasks = tasks.split(';;;')
+    for i in tasks:
+        isplited = i.split('--')
+        print(isplited)
+        all_tasks[isplited[0].strip('\n')] = [isplited[1].strip('\n'),
+                                              isplited[2].strip('\n'),
+                                              isplited[3].strip('\n'),
+                                              isplited[4].strip('\n')]
+                                #0 - номер задания, 1 - ответ, 2 - картинки, 3 - пояснение, 4 - само задание
+
+pprint(all_tasks)
+@bot.command(name='задание')
+async def tasks(ctx, number):
+    global current_task
+    one_task = []
+    for i in list(all_tasks):
+        if i[:i.find('.')] == number:
+            one_task.append(i)
+
+    current_task = random.choice(one_task)
+    pictures = all_tasks[current_task][1]
+    if pictures == 'none':
+        await ctx.send(all_tasks[current_task][3])
+    else:
+        await ctx.send('x')
+
+
+@bot.command(name='анекдот')
+async def stirlitz(ctx):
+    await ctx.send("https://phys-ege.sdamgia.ru/get_file?id=91721")
+
+
+@bot.event
+async def on_message(message):
+    global current_task
+    if message.author == bot.user or '!' in message.content:
+        return
+    if 'анекдот' in message.content:
+        await message.channel.send("а я тоже анекдотики знаю... Вот команда - !анекдот")
+        return
+
+    if message.content == all_tasks[current_task][0]:
+        await message.channel.send("правильно!")
+        await tasks(message.channel, current_task)
+        return
+    elif message.content == 'подсказка':
+        if all_tasks[current_task][2] == 'none':
+            await message.channel.send('подсказок к этому заданию нет.')
+            return
+        else:
+            await message.channel.send(all_tasks[current_task][2])
+            return
+    else:
+        await message.channel.send('неверно, попробуйте еще раз!')
+        return
+
+
+bot.run(TOKEN)
+
+
